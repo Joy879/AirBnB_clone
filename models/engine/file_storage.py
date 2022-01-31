f5249bd5051cca6d287595a24e00ca8fae668a75
root <root@kali>
2022-01-31 20:26:42 +0300

    [Update] : file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,49 +4,45 @@ Contains the class FileStorage
 """
 import json
 import os
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.state import State
-from models.review import Review
 
-        
+
 class FileStorage:
-    """
-    serializes instances to a JSON file and deserializes JSON file to instances
-    """
+    """ Class that serializes and deserializes JSON objects """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ returns the dictionary __objects
-        """
+        """ Returns the dictionary __objects """
         return FileStorage.__objects
 
     def new(self, obj):
-        """ sets in __objects the obj with key <obj class name>.id
-        """
+        """ Sets in __objects the obj with key <obj class name >.id """
         key = obj.__class__.__name__ + "." + obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """ serializes dict __objects to the JSON file
-        """
-        dict = {}
+        """ Serializes __objects to the JSON file """
+        dictionary = {}
+
         for key, value in FileStorage.__objects.items():
-            dict[key] = value.to_dict()
+            dictionary[key] = value.to_dict()
 
         with open(FileStorage.__file_path, 'w') as f:
-            json.dump(dict, f)
+            json.dump(dictionary, f)
 
     def reload(self):
-        """deserializes the JSON file to dict __objects
-        """
+        """ Deserializes __objects from the JSON file """
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.city import City
+        from models.amenity import Amenity
+        from models.state import State
+        from models.review import Review
         dct = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
                'City': City, 'Amenity': Amenity, 'State': State,
                'Review': Review}
+
         if os.path.exists(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, 'r') as f:
                 for key, value in json.load(f).items():
