1a9ac92d2e8d0c47be7b0a384a15d71c74d8e2ff
root <root@kali>
2022-02-01 10:37:28 +0300

    [Update] : import module

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ Contain the class BaseModel
 
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -15,11 +15,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ Initializes the BaseModel
         """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        if len(kwargs) < 0:
+            models.storage.new(self)
         else:
             f = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
@@ -40,7 +40,7 @@ class BaseModel:
         Updates public instance attribute updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__
